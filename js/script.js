05fceedccfc182902e049928d88a91bda7037153
Tetiana Savaryn <t.savaryn@gmail.com>
2023-03-17 19:28:20 -0400

    pagination works

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 // search
 const searchBtn = document.getElementById('search-btn');
-const resultItem = document.getElementById('result-item');
 const recipes = document.getElementById('recipes');
 const searchText = document.getElementById('search-text');
+const pagination = document.getElementById('pagination');
+const spoonacularKey = 'd03cd93b0d084c33b7afdf33bb873cdd';
+const resultsPerPage = 6;
+let numberOfResults;
 //const itemPhoto = document.getElementById('item-photo');
 //const itemName = document.getElementById('item-name');
 
@@ -13,9 +16,9 @@ searchBtn.addEventListener('click', function(event){
     searchInput += document.getElementById('search-input').value.trim();
     //let searchUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=73b9a76fc83e4d16a1e485b15383fc9f&query=${searchInput}`;
     //let searchUrl1 = `https://api.spoonacular.com/recipes/complexSearch?apiKey=73b9a76fc83e4d16a1e485b15383fc9f&query=pasta`;
-    let searchUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=e6f5e7b827d24fd9a71f7ec3bdb3d5b7&query=${searchInput}`;
+    let searchUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${spoonacularKey}&query=${searchInput}`;
     
-    fetchResults(searchUrl);
+    fetchResults(searchUrl, 1); // call fetchResults with pageNumber = 1
     
 });
 
@@ -23,7 +26,7 @@ searchBtn.addEventListener('click', function(event){
 //fetch test
 /*
 async function fetchResults(){
-    let searchUrl = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=73b9a76fc83e4d16a1e485b15383fc9f&query=pasta';
+    let searchUrl = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=71e4a356f7e44493bb3f45decfc5b3a7&query=pasta';
     let response = await fetch(searchUrl);
     console.log(response.status); // 200
     console.log(response.statusText); // OK
@@ -35,12 +38,19 @@ async function fetchResults(){
 fetchResults();
 */
 
-function fetchResults(searchUrl){
-    fetch(searchUrl)
+function fetchResults(searchUrl, pageNumber){
+    
+    let offset = (pageNumber - 1) * resultsPerPage; // calculate the offset based on pageNumber
+    let url = `${searchUrl}&offset=${offset}&number=${resultsPerPage}`; // limiting searchUrl to paginated
+    
+
+    fetch(url)
     .then(res => res.json())
     .then(data => {
         let html = "";
-        if(data.results.length>0) {
+        numberOfResults = JSON.parse(data.totalResults);
+        if(data.results) {
+            //console.log(numberOfResults);
             data.results.forEach(
                 result => {
                 html += `
@@ -60,18 +70,47 @@ function fetchResults(searchUrl){
 
                 `;
             });
+
+
+
+        // pagination links
+        let totalPages = Math.ceil(numberOfResults / resultsPerPage);
+        let paginationHtml = '';
+
+        for(let i = 1; i <= totalPages; i++){
+            if(i == pageNumber){
+                paginationHtml += `<div class="page-item active col text-align-left"><a class="page-link">${i}</a></div>`; //active page
+            }else{
+                paginationHtml += `<div class="page-item col text-align-left"><a href="#" class="page-link" data-page="${i}">${i}</a></div>`;
+            }
+        }
+        pagination.innerHTML = paginationHtml;
+        
+        // add event listeners to clicking page links
+        let paginationLinks = pagination.querySelectorAll('.page-link');
+        paginationLinks.forEach(link => {
+            link.addEventListener('click', function(event){
+                event.preventDefault();
+                let clickedPage = this.getAttribute('data-page');
+                fetchResults(searchUrl, clickedPage);
+            });
+        });
+
+
+
         } else {
             // if there's no results
             html = `<h2 style="text-align: center;">We couldn't find what you're looking for</h2>`
         }
         searchText.innerHTML = "Search Results:"
         recipes.innerHTML = html;
+        
 
     })
     .catch(rejected => {
         console.log(rejected);//handle the error
     })
+
+    
 }
-
-
 
